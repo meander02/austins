01cae4f432efa8206f0c1254bc936bd61b503ee3
meandro02 <rd209422@gmail.com>
2023-10-22 22:46:12 -0600

    efecto comp

--- src/app/core/components/politica/politica.component.ts
+++ src/app/core/components/politica/politica.component.ts
@@ -1,24 +1,46 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-
-// import { MatDialog } from '@angular/material/dialog';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { PoliticaPrivView } from '../../../features/portal/views/politica-priv/politica-priv.view';
 
 @Component({
   selector: 'app-politica',
   templateUrl: './politica.component.html',
-  styleUrls: ['./politica.component.scss']
+  styleUrls: ['./politica.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1600ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('1000ms ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('dialogFadeInOut', [
+      state('void', style({ transform: 'scale(0.5)', opacity: 0 })),
+      transition(':enter', [
+        animate('2000ms ease-in', style({ transform: 'scale(1)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ transform: 'scale(0.5)', opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class PoliticaComponent {
   isPrivacyPolicyVisible = true;
   dialogRef: MatDialogRef<PoliticaPrivView> | undefined;
+
   constructor(private dialog: MatDialog, private cdr: ChangeDetectorRef) {}
 
   openPrivacyPolicy() {
     this.isPrivacyPolicyVisible = true; // Mostrar el contenedor
     const dialogRef = this.dialog.open(PoliticaPrivView, {
-      width: '70%',
-      height: '70%'
+      width: '80%',
+      height: '70%',
+      panelClass: 'dialog-with-animation', // Clase para aplicar animación
     });
 
     dialogRef.afterOpened().subscribe(() => {
@@ -33,4 +55,3 @@ export class PoliticaComponent {
     this.isPrivacyPolicyVisible = false;
   }
 }
-
--- src/app/features/portal/views/politica-priv/politica-priv.view.ts
+++ src/app/features/portal/views/politica-priv/politica-priv.view.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-politica-priv',
   templateUrl: './politica-priv.view.html',
-  styleUrls: ['./politica-priv.view.scss']
+  styleUrls: ['./politica-priv.view.scss'],
+  animations: [
+    trigger('dialogFadeInOut', [
+      state('void',
+      style({ transform: 'scale(0)', opacity: 0 }
+      )
+      ),
+      transition(':enter', [
+        animate(
+          '300ms ease-in'
+          // style({
+          //   transform: 'scale(1)',
+          //   opacity: 1,
+          //   // scroll :-10,
+          // })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-out'
+          // style({ transform: 'scale(0.5)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
-export class PoliticaPrivView  {
+export class PoliticaPrivView
+{
+  showSeccionMas = false; // Inicialmente oculta
 
 }
