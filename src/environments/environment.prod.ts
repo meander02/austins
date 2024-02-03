9bdd8f57350dfe7d27d41c15aab6cf4e55a99789
meandro02 <rd209422@gmail.com>
2024-02-02 23:06:58 -0600

    gpt v12

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
   // ,api:'http://localhost:3000'
   ,api:'https://austin-b.onrender.com'
-  , apikey: "sk-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"
+  , apikey: "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"
 };
