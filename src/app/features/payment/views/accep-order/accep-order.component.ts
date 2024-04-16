bc25a102a57ce2835ca39ec869ad194f09d8d461
meandro02 <rd209422@gmail.com>
2024-04-16 09:07:54 -0600

    pedido 3

--- src/app/features/payment/views/accep-order/accep-order.component.ts
+++ src/app/features/payment/views/accep-order/accep-order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { OrderService } from '../../commons/services/order.service';
 
 @Component({
   selector: 'app-accep-order',
@@ -9,8 +10,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AccepOrderComponent implements OnInit {
   SEMUYI: string = "";
+  pedidoInfo: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+  constructor(
+    private orderService: OrderService,
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
     // Obtener el parámetro SEMUYI de la URL
@@ -27,10 +33,23 @@ export class AccepOrderComponent implements OnInit {
       (response: any) => {
         console.log('Respuesta de la consulta:', response);
         // Aquí puedes manejar la respuesta según sea necesario
+        this.pedidoInfo = response;
       },
       (error) => {
         console.error('Error al realizar la consulta:', error);
         // Aquí puedes manejar los errores según sea necesario
+      }
+    );
+
+    this.orderService.consultarPedido(SEMUYI).subscribe(
+      (response) => {
+        console.log(response);
+        // Verificar la estructura de la respuesta
+        // if ('resultado' in response && 'codigoPedido' in response.resultado) {
+        this.pedidoInfo = response;
+      },
+      (error) => {
+        console.error('Error al consultar pedido:', error);
       }
     );
   }
