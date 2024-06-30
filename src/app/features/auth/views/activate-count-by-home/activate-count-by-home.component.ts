68f6af59d2dca1f13b2ca079097e437ccbe1569d
meandro02 <rd209422@gmail.com>
2024-06-30 02:05:15 -0600

    modal

--- src/app/features/auth/views/activate-count-by-home/activate-count-by-home.component.ts
+++ src/app/features/auth/views/activate-count-by-home/activate-count-by-home.component.ts
@@ -37,13 +37,18 @@ export class ActivateCountByHomeComponent {
             this.success = 'Correo de activación enviado correctamente';
             this.showRecoverLink = false;
             console.log('Correo de activación enviado correctamente');
-            this.messageService.add({ severity: 'success', summary: 'Éxito', detail: 'Correo de activación enviado correctamente' });
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Éxito',
+              detail: 'Correo de activación enviado correctamente',
+              life: 2000 // 2000 milisegundos = 2 segundos
+            });
             this.ngxLoader.stop();
 
             // Mostrar el mensaje durante 2 segundos antes de cerrar el diálogo
             setTimeout(() => {
               this.dialogRefService.closeDialog();
-            }, 5000); // 2000 milisegundos = 2 segundos
+            }, 8000); // 2000 milisegundos = 2 segundos
           },
           (error: HttpErrorResponse) => {
             if (error.status === 400) {
@@ -54,6 +59,7 @@ export class ActivateCountByHomeComponent {
               this.showRecoverLink = false;
             }
             this.messageService.add({ severity: 'error', summary: 'Error', detail: this.error || undefined });
+            this.ngxLoader.stop();
           }
         );
     }
